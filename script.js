e11697d299052723fd5f5825fc8cd22efc884d3a
Bagas Aditiya <bagasaditiya@Bagas-Aditiya.local>
2025-05-23 21:44:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("connectKontak");
+
+  button.addEventListener("click", function () {
+    window.open("https://wa.me/081293277441", "_blank");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("connectBtn");
+  const target = document.getElementById("connect");
+
+  button.addEventListener("click", function () {
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
